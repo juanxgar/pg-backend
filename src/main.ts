91ea4fde1c261510471e9263e43dc642d92b375b
juanxgar <juandiego0827@gmail.com>
2023-03-20 12:34:46 -0500

    Feat: Prisma setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('DOCUMENTACIÓN')
+    .setDescription(
+      'Documentación de proyecto para gestión de estudiantes de medicina en internado',
+    )
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+  const document = SwaggerModule.createDocument(app, config, options);
+
+  SwaggerModule.setup('api_documentation', app, document);
+
+  await app.listen(8000);
 }
 bootstrap();
